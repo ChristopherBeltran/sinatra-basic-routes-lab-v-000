60a13ad07052243cc2ad1c8b537fdf9189ebcb09
Christopher Beltran <christod.beltran@gmail.com>
2018-11-19 06:57:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,13 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Chris"
   end 
+  
+  get '/hometown' do 
+    "My hometown is Los Angeles"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is roulette dares"
+  end 
+  
 end
